dc473992ce4ace260c956e0ce9d18f81b0635f2e
arjun-sudo <laserarjun@gmail.com>
2020-07-16 23:57:31 +0545

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(
+//  React.createElement("h1",{style:{color:"blue"}},"Hello!"),
+//   document.getElementById('root')
+// );
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+<ul>
+  <li>Green Tea</li>
+  <li>Black Tea</li>
+  <li>Milk Tea</li>
+  <li>Lomon Tea</li>
+</ul>
+ );
+ 
+//  Use babaljs to see the componets

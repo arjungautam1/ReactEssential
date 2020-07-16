f22d24a594671b75fb75fe277a87eb7c4a1a56ca
arjun-sudo <laserarjun@gmail.com>
2020-07-17 00:14:10 +0545

    Jsx

--- src/index.js
+++ src/index.js
@@ -7,13 +7,25 @@ import './index.css';
 //   document.getElementById('root')
 // );
 
-ReactDOM.render(
-<ul>
-  <li>Green Tea</li>
-  <li>Black Tea</li>
-  <li>Milk Tea</li>
-  <li>Lomon Tea</li>
-</ul>
+// ReactDOM.render(
+// <ul>
+//   <li>Green Tea</li>
+//   <li>Black Tea</li>
+//   <li>Milk Tea</li>
+//   <li>Lomon Tea</li>
+// </ul>
+//  );
+ let city={
+   name:"Kathmandu",
+   country:"Nepal"
+ };
+
+ ReactDOM.render(
+   <h1 id="heading" className="cool-text">
+     {city.name} is in {city.country}
+   </h1>,
+   document.getElementById("root")
+
  );
  
 //  Use babaljs to see the componets

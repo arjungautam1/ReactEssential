a4dc272b3e650723fe8b85bf6c229d36e808ee07
arjun-sudo <laserarjun@gmail.com>
2020-07-17 09:11:12 +0545

    properties

--- src/index.js
+++ src/index.js
@@ -30,22 +30,68 @@ import './index.css';
 
 //  );
 
+//  Use babaljs to see the componets
 
 // Componets 
 
-function Hello(){ //Should be always capital
+// function Hello(){ //Should be always capital
+//   return(
+//     <div>
+//       <h1>Welcome to React !</h1>
+//       <p>Let's build something cool.</p>
+//     </div>
+//   );
+
+// }
+
+// ReactDOM.render(
+//   <Hello/>,
+//   document.getElementById("root")
+// );
+ 
+
+// Properties
+
+// function Hello(props)
+// {
+//   console.log(props)
+//   return(
+//     <div>
+//       <h1>Welcome to {props.library} !</h1>
+//       <p>{props.message}</p>
+//       <p>{Object.keys(props).length}  Props Total</p>
+//     </div>
+//   );
+// }
+
+//   ReactDOM.render(
+//     <Hello library="React.js" 
+//     message="Have fun !"
+//     number={3}
+//     />,
+//     document.getElementById("root")
+//   );
+
+
+// Properties in more cleaner way
+
+function Hello({library,message,number})
+{
+  
   return(
     <div>
-      <h1>Welcome to React !</h1>
-      <p>Let's build something cool.</p>
+      <h1>Welcome to {library} World !</h1>
+      <p>{message}</p>
+      <p>{number} Total props</p>
     </div>
   );
-
 }
 
 ReactDOM.render(
-  <Hello/>,
+  <Hello
+  library="React"
+  message="Have Fun !"
+  number={3}
+  />,
   document.getElementById("root")
 );
- 
-//  Use babaljs to see the componets
